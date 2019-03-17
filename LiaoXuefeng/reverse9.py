bc527b6f5d6fb6ee934fd75420588a6919159389
tom_glb <goodMorning_glb@atguigu.com>
2019-03-17 19:58:20 +0800

    数值逆置：int(str(i)[::-1])

--- LiaoXuefeng/reverse9.py
+++ LiaoXuefeng/reverse9.py
@@ -1,15 +1,20 @@
 '''
     设N是一个四位数，它的9倍恰好是其反序数（例如：1234 的反序数是4321），求N的值。
 '''
-def number(n):
-    c = 0
-    while (n > 0):
-        a = n % 10
-        n = n // 10
-        c = c * 10 + a
-    return c
+# def number(n):
+#     c = 0
+#     while (n > 0):
+#         a = n % 10
+#         n = n // 10
+#         c = c * 10 + a
+#     return c
+#
+# for i in range(1000, 10000):
+#     n = number(i)
+#     if i * 9 == n:
+#         print(i)
 
+'''牛人：将数值转为字符串，切片完成！'''
 for i in range(1000, 10000):
-    n = number(i)
-    if i * 9 == n:
+    if int(str(i)[::-1]) == i * 9:
         print(i)
