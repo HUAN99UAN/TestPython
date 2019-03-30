9f91c85e3dbbb45e0a7c63bc5ddd6dabab165e32
tom_glb <goodMorning_glb@atguigu.com>
2019-03-30 21:29:16 +0800

    20190330

--- daily_programming/some_sort_method/bubble_sort.py
+++ daily_programming/some_sort_method/bubble_sort.py
@@ -9,8 +9,27 @@ def bubble_sort(aList):
             if aList[j] < aList[j+1]:
                 aList[j], aList[j+1] = aList[j+1], aList[j]
 
+#  改进：设立tag标记一趟冒泡是否有交换，没有说明已经有序！
+def bubble_sort2(aList):
+    tag = 0
+    n = len(aList)
+    for i in range(0, n-1):
+        tag = 0
+        for j in range(0, n-i-1):
+            if aList[j] < aList[j+1]:
+                aList[j], aList[j+1] = aList[j+1], aList[j]
+                tag = 1
+        if tag == 0:
+            break
+
 if __name__ == '__main__':
     li = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    # li = [9, 8, 7, 6, 5, 4, 3, 2, 1]
     print(li)
     bubble_sort(li)
+    print(li)
+
+    li = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    print(li)
+    bubble_sort2(li)
     print(li)
---
+++ daily_programming/some_sort_method/shell's_sort.py
@@ -0,0 +1,22 @@
+'''
+希尔排序：小块的插入排序！
+'''
+
+def shell_sort(aList):
+    n = len(aList)
+    step = n // 2 # 增量
+    while step > 0:
+        for i in range(step, n): # 各子部分插入排序
+            while i >= step and aList[i] < aList[i-step]:
+                aList[i], aList[i-step] = aList[i-step], aList[i]
+                i -= step
+        print('排序中间过程：', aList)
+        step = step //2
+
+
+if __name__ == '__main__':
+    # li = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    li = [49, 38, 65, 97, 76, 13, 27, 48, 55, 4]
+    print(li)
+    shell_sort(li)
+    print(li)
